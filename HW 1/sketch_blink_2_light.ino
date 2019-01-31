dbff5e04e3d84dab1a95bd70a9ec9db48f4196af
kaiwenliu96 <47086936+kaiwenliu96@users.noreply.github.com>
2019-01-31 08:28:28 -0800

    2LED Code ZIP
    2 LED Code with Zip

---
+++ HW 1/sketch_blink_2_light.ino
@@ -0,0 +1,36 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7,HIGH);
+  delay(250);
+  digitalWrite(7,LOW);
+  delay(250);
+  digitalWrite(8,HIGH);
+  delay(250);
+  digitalWrite(8,LOW);
+  delay(250);
+  digitalWrite(7,HIGH);
+  delay(250);
+  digitalWrite(7,LOW);
+  delay(250);
+  digitalWrite(8,HIGH);
+  delay(250);
+  digitalWrite(8,LOW);
+  delay(250);
+  digitalWrite(7,HIGH);
+  delay(250);
+  digitalWrite(7,LOW);
+  delay(250);
+  digitalWrite(8,HIGH);digitalWrite(7,HIGH);
+  delay(2000);
+  
+
+  
+
+}
